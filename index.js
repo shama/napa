e50d17cab21c64ecb760682ddd786a1c41d67155
Kyle Robinson Young <kyle@dontkry.com>
2013-11-13 19:56:51 -0800

    Ability to set packages using napa key in package.json

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var spawn = require('child_process').spawn
 var path = require('path')
+var fs = require('fs')
 var log = require('npmlog')
 var rimraf = require('rimraf')
 var cwd = process.cwd()
@@ -12,10 +13,13 @@ napa.cli = function(args, done) {
     if (total < 1) return done()
     total--
   }
-  args.map(napa.args).map(napa.cmd).forEach(function(cmd) {
+  pkg = napa.readpkg()
+  if (pkg) args = args.map(napa.args).concat(pkg)
+  else args = args.map(napa.args)
+  args.map(napa.cmd).forEach(function(cmd) {
     total++
     log.info('install', '%s into %s', cmd[cmd.length-2], path.relative(cwd, cmd[cmd.length-1]))
-    rimraf(cmd[3], function(err) {
+    rimraf(cmd[cmd.length-1], function(err) {
       if (err) return log.error(err)
       var git = spawn(cmd[0], cmd.slice(1))
       git.stderr.on('data', log.error)
@@ -42,12 +46,25 @@ napa.args = function(str) {
   }
 
   if (!name) name = url.slice(url.lastIndexOf('/') + 1)
+  return [napa.url(url), name]
+}
+
+napa.url = function(url) {
   if (url.slice(0, 1) === '/') url = url.slice(1)
   if (url.indexOf('://') === -1) url = 'git://github.com/' + url
-
-  return [url, name]
+  return url
 }
 
 napa.cmd = function(repo) {
   return ['git', 'clone', '--depth', '1', repo[0], path.join(cwd, 'node_modules', repo[1])]
+}
+
+napa.readpkg = function() {
+  pkg = path.join(cwd, 'package.json')
+  if (!fs.existsSync(pkg)) return false
+  pkg = require(pkg)
+  if (!pkg.hasOwnProperty('napa')) return false
+  return Object.keys(pkg.napa).map(function(key) {
+    return [napa.url(pkg.napa[key]), key]
+  })
 }
--- test.js
+++ test.js
@@ -15,3 +15,10 @@ test('cmds', function(t) {
   cmd[cmd.length-1] = path.relative(process.cwd(), cmd[cmd.length-1])
   t.deepEqual(cmd, ['git', 'clone', '--depth', '1', 'git://github.com/user/repo', 'node_modules/test'])
 })
+
+test('readpkg', function(t) {
+  t.plan(1)
+  var actual = napa.readpkg()
+  var expected = [['git://github.com/foo/repo', 'foo'], ['git://example.com/bar/repo', 'bar']]
+  t.deepEqual(actual, expected)
+})
