a7bc11a8dbc02c0fe536b9c43e6263b705ea763e
Kyle Robinson Young <kyle@dontkry.com>
2014-01-24 09:44:33 -0800

    Ability to download packages using any URL. Closes GH-1, Closes GH-2.

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var path = require('path')
 var fs = require('fs')
 var log = require('npmlog')
 var rimraf = require('rimraf')
+var download = require('download')
 var cwd = process.cwd()
 
 var napa = module.exports = {}
@@ -21,9 +22,17 @@ napa.cli = function(args, done) {
     log.info('install', '%s into %s', cmd[cmd.length-2], path.relative(cwd, cmd[cmd.length-1]))
     rimraf(cmd[cmd.length-1], function(err) {
       if (err) return log.error(err)
-      var git = spawn(cmd[0], cmd.slice(1))
-      git.stderr.on('data', log.error)
-      git.on('close', close)
+      if (cmd[0] === 'git') {
+        // Download with git
+        var git = spawn(cmd[0], cmd.slice(1))
+        git.stderr.on('data', log.error)
+        git.on('close', close)
+      } else {
+        // Download with download
+        var d = download(cmd[1], cmd[2], { extract: true })
+        d.on('error', log.error)
+        d.on('close', close)
+      }
     })
   })
 }
@@ -50,13 +59,27 @@ napa.args = function(str) {
 }
 
 napa.url = function(url) {
+  if (url.indexOf('#') !== -1 && url.indexOf('://') === -1) {
+    var s = url.split('#')
+    url = 'https://github.com/' + s[0] + '/archive/' + s[1]
+    if (process.platform === 'win32') url += '.zip'
+    else url += '.tar.gz'
+  }
   if (url.slice(0, 1) === '/') url = url.slice(1)
   if (url.indexOf('://') === -1) url = 'git://github.com/' + url
   return url
 }
 
 napa.cmd = function(repo) {
-  return ['git', 'clone', '--depth', '1', repo[0], path.join(cwd, 'node_modules', repo[1])]
+  var outpath = path.join(cwd, 'node_modules', repo[1])
+  if (repo[0].slice(0, 6) === 'git://') {
+    return ['git', 'clone', '--depth', '1', repo[0], outpath]
+  } else {
+    if (repo[0].indexOf('github.com') !== -1 && repo[0].indexOf('/archive/') === -1) {
+      return ['git', 'clone', '--depth', '1', repo[0], outpath]
+    }
+    return ['download', repo[0], outpath]
+  }
 }
 
 napa.readpkg = function() {
--- test.js
+++ test.js
@@ -3,17 +3,27 @@ var test = require('tape')
 var path = require('path')
 
 test('args', function(t) {
-  t.plan(3)
+  t.plan(5)
   t.deepEqual(napa.args('user/repo'), ['git://github.com/user/repo', 'repo'])
   t.deepEqual(napa.args('https://github.com/user/repo:testing'), ['https://github.com/user/repo', 'testing'])
   t.deepEqual(napa.args('git://github.com/user/repo2'), ['git://github.com/user/repo2', 'repo2'])
+  t.deepEqual(napa.args('angular/angular.js#v1.2.3:angular'), ['https://github.com/angular/angular.js/archive/v1.2.3.tar.gz', 'angular'])
+  t.deepEqual(napa.args('https://github.com/angular/angular.js/archive/master.zip:angular'), ['https://github.com/angular/angular.js/archive/master.zip', 'angular'])
 })
 
 test('cmds', function(t) {
-  t.plan(1)
+  t.plan(3)
   var cmd = napa.cmd(['git://github.com/user/repo', 'test'])
   cmd[cmd.length-1] = path.relative(process.cwd(), cmd[cmd.length-1])
   t.deepEqual(cmd, ['git', 'clone', '--depth', '1', 'git://github.com/user/repo', 'node_modules/test'])
+
+  cmd = napa.cmd(['https://github.com/user/repo', 'test'])
+  cmd[cmd.length-1] = path.relative(process.cwd(), cmd[cmd.length-1])
+  t.deepEqual(cmd, ['git', 'clone', '--depth', '1', 'https://github.com/user/repo', 'node_modules/test'])
+
+  cmd = napa.cmd(['https://github.com/angular/angular.js/archive/master.zip', 'angular'])
+  cmd[cmd.length-1] = path.relative(process.cwd(), cmd[cmd.length-1])
+  t.deepEqual(cmd, ['download', 'https://github.com/angular/angular.js/archive/master.zip', 'node_modules/angular'])
 })
 
 test('readpkg', function(t) {
