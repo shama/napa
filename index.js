63ab5bcd90ea5ba298bdacc27bd4a8ab7d76f8e6
Kyle Robinson Young <kyle@dontkry.com>
2014-10-08 12:29:25 -0700

    Install from cache, avoid duplicate installs. Fixes GH-24. Fixes GH-14.

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
-var spawn = require('child_process').spawn
 var path = require('path')
 var fs = require('fs')
-var log = require('npmlog')
-var rimraf = require('rimraf')
-var download = require('download')
 var cwd = process.cwd()
+var Pkg = require('./lib/pkg')
 
 var napa = module.exports = {}
 
@@ -17,24 +14,10 @@ napa.cli = function(args, done) {
   pkg = napa.readpkg()
   if (pkg) args = args.map(napa.args).concat(pkg)
   else args = args.map(napa.args)
-  args.map(napa.cmd).forEach(function(cmd) {
+  args.forEach(function(cmd) {
     total++
-    log.info('install', '%s into %s', cmd[cmd.length-2], path.relative(cwd, cmd[cmd.length-1]))
-    rimraf(cmd[cmd.length-1], function(err) {
-      if (err) return log.error(err)
-      if (cmd[0] === 'git') {
-        // Download with git
-        cmd.push('-q')
-        var git = spawn(cmd[0], cmd.slice(1))
-        git.stderr.on('data', log.error)
-        git.on('close', close)
-      } else {
-        // Download with download
-        var d = download(cmd[1], cmd[2], { extract: true, strip: 1 })
-        d.on('error', log.error)
-        d.on('close', close)
-      }
-    })
+    var pkg = new Pkg(cmd[0], cmd[1])
+    pkg.install(close)
   })
 }
 
@@ -60,6 +43,10 @@ napa.args = function(str) {
 }
 
 napa.url = function(url) {
+  if (typeof url !== 'string') {
+    if (url.url) url = url.url
+    else return false
+  }
   if (url.indexOf('#') !== -1 && url.indexOf('://') === -1) {
     var s = url.split('#')
     url = 'https://github.com/' + s[0] + '/archive/' + s[1]
@@ -69,26 +56,6 @@ napa.url = function(url) {
   if (url.slice(0, 1) === '/') url = url.slice(1)
   if (url.indexOf('://') === -1) url = 'git://github.com/' + url
   return url
-}
-
-napa.cmd = function(repo) {
-  var outpath = path.join(cwd, 'node_modules', repo[1])
-  
-  // is this a git repo url?
-  var gitUrls = ['git+', 'git://']
-  var url = repo[0]
-  
-  // all git urls need to have the git+ stripped
-  var gitCmd = ['git', 'clone', '--depth', '1', (repo[0].replace('git+', '')), outpath]
-
-  if (gitUrls.indexOf(url.slice(0, 4)) !== -1) {
-    return gitCmd
-  } else {
-    if (url.indexOf('github.com') !== -1 && url.indexOf('/archive/') === -1) {
-      return gitCmd
-    }
-    return ['download', url, outpath]
-  }
 }
 
 napa.readpkg = function() {
--- test.js
+++ test.js
@@ -1,6 +1,20 @@
-var napa = require('./')
+var napa = require('./index.js')
+var Pkg = require('./lib/pkg')
 var test = require('tape')
 var path = require('path')
+var fs = require('fs')
+var rimraf = require('rimraf')
+
+function clean(filepaths, done) {
+  var count = filepaths.length
+  function cb() {
+    count--
+    if (count < 1) process.nextTick(done)
+  }
+  for (var i = 0; i < filepaths.length; i++) {
+     rimraf(filepaths[i], cb)
+  }
+}
 
 test('args', function(t) {
   t.plan(5)
@@ -14,43 +28,90 @@ test('args', function(t) {
 
 test('cmds', function(t) {
   t.plan(7);
-  var testPath = path.join('node_modules', 'test');
+  var testPath = path.resolve('node_modules', 'test');
+  var pkg = null
+
+  function assertPkg(url, name, cb) {
+    pkg = new Pkg(url, name, { _mock: cb })
+    pkg.install()
+  }
 
   // if github is in the url then its treated like a git+ url
-  var cmd = napa.cmd(['git://github.com/user/repo', 'test'])
-  cmd[cmd.length - 1] = path.relative(process.cwd(), cmd[cmd.length - 1])
-  t.deepEqual(cmd, ['git', 'clone', '--depth', '1', 'git://github.com/user/repo', testPath])
-
-  cmd = napa.cmd(['https://github.com/user/repo', 'test'])
-  cmd[cmd.length - 1] = path.relative(process.cwd(), cmd[cmd.length - 1])
-  t.deepEqual(cmd, ['git', 'clone', '--depth', '1', 'https://github.com/user/repo', testPath])
+  assertPkg('git://github.com/user/repo', 'test', function(result) {
+    t.deepEqual(result, ['git', ['clone', '--depth', '1', '-q', 'git://github.com/user/repo', testPath]])
+  })
+  assertPkg('https://github.com/user/repo', 'test', function(result) {
+    t.deepEqual(result, ['git', ['clone', '--depth', '1', '-q', 'https://github.com/user/repo', testPath]])
+  })
 
   // git+ works like npm install see https://www.npmjs.org/doc/cli/npm-install.html
-  cmd = napa.cmd(['git+http://test.com/user/repo', 'test'])
-  cmd[cmd.length - 1] = path.relative(process.cwd(), cmd[cmd.length - 1])
-  t.deepEqual(cmd, ['git', 'clone', '--depth', '1', 'http://test.com/user/repo', testPath])
- 
-  cmd = napa.cmd(['git+https://test.com/user/repo', 'test'])
-  cmd[cmd.length - 1] = path.relative(process.cwd(), cmd[cmd.length - 1])
-  t.deepEqual(cmd, ['git', 'clone', '--depth', '1', 'https://test.com/user/repo', testPath])
-
-  cmd = napa.cmd(['git+ssh://test.com/user/repo', 'test'])
-  cmd[cmd.length - 1] = path.relative(process.cwd(), cmd[cmd.length - 1])
-  t.deepEqual(cmd, ['git', 'clone', '--depth', '1', 'ssh://test.com/user/repo', testPath])
+  assertPkg('git+http://test.com/user/repo', 'test', function(result) {
+    t.deepEqual(result, ['git', ['clone', '--depth', '1', '-q', 'http://test.com/user/repo', testPath]])
+  })
+  assertPkg('git+https://test.com/user/repo', 'test', function(result) {
+    t.deepEqual(result, ['git', ['clone', '--depth', '1', '-q', 'https://test.com/user/repo', testPath]])
+  })
+  assertPkg('git+ssh://test.com/user/repo', 'test', function(result) {
+    t.deepEqual(result, ['git', ['clone', '--depth', '1', '-q', 'ssh://test.com/user/repo', testPath]])
+  })
 
   // download
-  cmd = napa.cmd(['ssh://test.com/user/repo', 'test'])
-  cmd[cmd.length - 1] = path.relative(process.cwd(), cmd[cmd.length - 1])
-  t.deepEqual(cmd, ['download', 'ssh://test.com/user/repo', testPath])
-
-  cmd = napa.cmd(['https://github.com/angular/angular.js/archive/master.zip', 'angular'])
-  cmd[cmd.length - 1] = path.relative(process.cwd(), cmd[cmd.length - 1])
-  t.deepEqual(cmd, ['download', 'https://github.com/angular/angular.js/archive/master.zip', 'node_modules' + path.sep + 'angular'])
+  assertPkg('ssh://test.com/user/repo', 'test', function(result) {
+    t.deepEqual(result, ['download', 'ssh://test.com/user/repo', testPath])
+  })
+  assertPkg('https://github.com/angular/angular.js/archive/master.zip', 'angular', function(result) {
+    t.deepEqual(result, ['download', 'https://github.com/angular/angular.js/archive/master.zip', path.resolve('node_modules', 'angular')])
+  })
 })
 
 test('readpkg', function(t) {
   t.plan(1)
   var actual = napa.readpkg()
-  var expected = [['git://github.com/foo/repo', 'foo'], ['https://github.com/emberjs/ember.js/archive/v1.5.0-beta.1.tar.gz', 'ember']]
+  var expected = [
+    ['git://github.com/foo/repo', 'foo'],
+    ['https://github.com/emberjs/ember.js/archive/v1.7.0.tar.gz', 'ember'],
+    ['git://github.com/components/handlebars.js', 'handlebars'],
+  ]
   t.deepEqual(actual, expected)
+})
+
+test('pkg install', function(t) {
+  t.plan(8)
+  var url = 'https://github.com/emberjs/ember.js/archive/v1.7.0.tar.gz'
+  var pkgName = 'ember'
+  var pkg = new Pkg(url, pkgName)
+  clean([pkg.cacheTo, pkg.installTo], function() {
+    pkg.install(function() {
+      t.ok(fs.existsSync(pkg.installTo), 'file was installed to node_modules')
+      t.ok(pkg.installed, 'pkg says it was installed')
+      t.ok(fs.existsSync(pkg.cacheTo), 'file was cached')
+      t.ok(pkg.cached, 'pkg says it was cached')
+      // Delete pkg and install again
+      clean([pkg.installTo], function() {
+        pkg = new Pkg(url, pkgName)
+        t.ok(!pkg.installed, 'pkg says not installed after deleted')
+        t.ok(fs.existsSync(pkg.cacheTo), 'pkg deleted but cache remains')
+        pkg.install(function() {
+          t.ok(fs.existsSync(pkg.installTo), 'pkg installed from cache')
+          t.ok(pkg.installed, 'pkg says installed when from cache')
+        })
+      })
+    })
+  })
+})
+
+test('pkg install different version', function(t) {
+  t.plan(2)
+  var result = null
+  var pkg = new Pkg('https://github.com/emberjs/ember.js/archive/v1.6.0.tar.gz', 'ember')
+  pkg.install(function() {
+    result = require(path.resolve(pkg.installTo, 'package.json'))[pkg._napaResolvedKey]
+    t.equal(result, 'https://github.com/emberjs/ember.js/archive/v1.6.0.tar.gz', 'should have installed the older version')
+
+    pkg = new Pkg('https://github.com/emberjs/ember.js/archive/v1.7.0.tar.gz', 'ember')
+    pkg.install(function() {
+      result = require(path.resolve(pkg.installTo, 'package.json'))[pkg._napaResolvedKey]
+      t.equal(result, 'https://github.com/emberjs/ember.js/archive/v1.7.0.tar.gz', 'should have installed the newer version')
+    })
+  })
 })
